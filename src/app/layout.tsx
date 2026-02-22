308c4c1774e991920e421fa4daa4d05b2422ad7b
Rafael Machado Ampudia <rafael.machado.ampudia01@gmail.com>
2026-02-22 13:56:59 -0300

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,14 @@
 import "@/app/styles/globals.css";
 import Background from '@/app/components/ui/BackGround'; 
+import { title } from "process";
+
+export const metadata = {
+  title: 'ondeFica?',
+  description: 'Descubra endereços pelo Cep',
+  icons: {
+    icon: "/favicon.svg"
+  }
+}
 
 export default function RootLayout({
   children,
