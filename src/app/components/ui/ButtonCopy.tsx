7e650ebcb278b2ef579ccf49c572204d72c629ce
Rafael Machado Ampudia <rafael.machado.ampudia01@gmail.com>
2026-02-27 05:26:40 -0300

    fixed bad comment

--- src/app/components/ui/ButtonCopy.tsx
+++ src/app/components/ui/ButtonCopy.tsx
@@ -18,7 +18,7 @@ export default function ButtonCopy({content,icon,onClick,variant}: ButtonProps) 
     const Icon = icon; // Referenciando que o componente icon é o icon de lucideIcon.
   return (
     <button onClick={onClick} className={` ${variants[variant]} flex items-center gap-2 p-3 rounded-xl mx-auto transition-colors duration-300 hover:cursor-pointer`}>
-      <Icon color="white" size={18}/> {content} // Referenciando para que podemos usar o size e color.
+      <Icon color="white" size={18}/> {content} {/*Referenciando para que podemos usar o size e color. */} 
       </button>
   )
 }
